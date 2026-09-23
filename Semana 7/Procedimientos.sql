6b9c5cbaf465a74c84999f51eb17156150eb7fde
Felipe Linnebrink <fbarra1994@gmail.com>
2026-09-23 09:13:57 -0300

    Procedimientos

---
+++ Semana 7/Procedimientos.sql
@@ -0,0 +1,40 @@
+DROP PROCEDURE Insertar_cliente;
+
+CREATE OR REPLACE PROCEDURE Insertar_cliente(
+    p_rut IN VARCHAR2,
+    p_nombre IN VARCHAR2,
+    p_apellido IN VARCHAR2,
+    p_email IN VARCHAR2,
+    p_telefono IN VARCHAR2
+)
+--Las Variables son declaradas despues del AS, y antes del BEGIN 
+AS
+
+BEGIN
+    INSERT INTO CLIENTE (RUT, NOMBRE, APELLIDO, EMAIL, TELEFONO)VALUES (p_rut, p_nombre,
+     p_apellido, p_email, p_telefono);
+
+    COMMIT;
+    DBMS_OUTPUT.PUT_LINE('Usuario Registrado Correctamente.');
+
+END Insertar_cliente;
+/
+
+--Trabajo en Clase:
+--crear un procedimiento que retorne los datos de un cliente segun su ID 
+CREATE OR REPLACE PROCEDURE BUSCAR_CLIENTE_POR_ID(
+    p_cliente_id IN NUMBER,
+    p_nombre OUT VARCHAR2,
+    p_apellido OUT VARCHAR2,
+    p_email OUT VARCHAR2,
+    p_telefono OUT VARCHAR2
+) IS
+
+BEGIN
+    SELECT  NOMBRE, APELLIDO, EMAIL, TELEFONO
+    INTO  p_nombre, p_apellido, p_email, p_telefono
+    FROM CLIENTE
+    WHERE ID = p_cliente_id;
+    dbms_output.put_line('Cliente encontrado: ' || p_nombre || ' ' || p_apellido);
+END BUSCAR_CLIENTE_POR_ID;
+/
---
+++ Semana 7/sandbox.sql
@@ -0,0 +1,22 @@
+
+
+BEGIN
+    Insertar_cliente('14.201.255-7', 'Miguel', 'Sanhueza', 'msanhueza@gmail.com', null);
+
+END;
+/
+
+
+
+-- BUSCAR PO RLCIENTE POR ID, REGRESSA PARAMETROS DE SALIDA
+DECLARE
+    v_nombre CLIENTE.NOMBRE%TYPE;
+    v_apellido CLIENTE.APELLIDO%TYPE;
+    v_email CLIENTE.EMAIL%TYPE;
+    v_telefono CLIENTE.TELEFONO%TYPE;
+BEGIN
+    BUSCAR_CLIENTE_POR_ID(1, v_nombre, v_apellido, v_email, v_telefono);
+    dbms_output.put_line('Cliente ' || v_nombre || ' ' || v_apellido || ' Email: ' || v_email || ' Telefono: ' || v_telefono);
+
+END;
+/
